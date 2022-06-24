88b45937efb8f2c9b033b96e2da81fb6552cc477
Cyber <65469887+Cyber-Woxy@users.noreply.github.com>
2022-06-24 22:22:17 +0300

    50 star 20 fork düdükler
    50 star 20 fork düdükler

---
+++ spammer.py
@@ -0,0 +1,16 @@
+import requests
+
+from lxml.html import fromstring
+from itertools import cycle
+from utils import *
+
+
+
+anaurl = URLCİK
+sunucuidsi = SUNUCUİD
+webhook = WEBHOOKCUK
+token = TOKEN
+
+
+
+50 STAR 20 FORKTA GELİR DEVAMI KERANECİLER
